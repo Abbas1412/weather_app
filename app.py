484f36a36b29780570f6f14b77deffaa3eb0a32b
Abbas1412 <zaminabbas084@gmail.com>
2023-10-29 03:21:48 +0530

    My First Weather API

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route("/weatherapp", methods = ['POST'])
+def get_weather_data():
+
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    param = {'q': request.form.get("city"),
+             'units' : request.form.get("units"),
+              'appid': request.form.get("appid") }
+
+
+    response = requests.get(url, params = param)
+    data = response.json()
+    return f"Data: {data}"
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0")
